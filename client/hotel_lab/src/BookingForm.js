13bda52eea7f1aa16302bb85ca340d00c8232f1f
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-07-12 13:47:36 +0100

    removed node modules etc

--- client/hotel_lab/src/BookingForm.js
+++ client/hotel_lab/src/BookingForm.js
@@ -1,7 +1,7 @@
-import {useState} from 'react';
-import {postBooking} from './BookingService';
+import { useState } from 'react';
+import { postBooking } from './BookingService';
 
-const BookingForm = ({addBooking}) => {
+const BookingForm = ({ addBooking }) => {
     const [formData, setFormData] = useState({});
 
     const handleSubmit = (e) => {
@@ -10,21 +10,21 @@ const BookingForm = ({addBooking}) => {
         postBooking(formData).then((data) => {
             addBooking(data)
         })
-      e.target.reset();
+        e.target.reset();
     }
-    
+
     const onChange = (e) => {
         formData[e.target.id] = e.target.value;
         setFormData(formData);
     };
 
     return (
-        <form className="BookingForm"onSubmit={handleSubmit} method ="post">
+        <form className="BookingForm" onSubmit={handleSubmit} method="post">
             <label htmlFor="guestName"> Guest Name </label>
-            <input onChange={onChange} type ="text" id="guestName" v-model="guestName" required/>
+            <input onChange={onChange} type="text" id="guestName" v-model="guestName" required />
             <label htmlFor="guestEmail">Guest Email </label>
-            <input onChange={onChange} type ="text" id="guestEmail" v-model="guestEmail" required/>
-            <input type="submit" value="Save" id="save"/>
+            <input onChange={onChange} type="text" id="guestEmail" v-model="guestEmail" required />
+            <input type="submit" value="Save" id="save" />
         </form>
     )
 
